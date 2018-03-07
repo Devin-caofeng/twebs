5edcd6a9cd7a1c6b9cccad3a1f6442a8ad0dc1d7
devincaofeng <devin.caofeng@gmail.com>
2018-03-07 18:47:48 +0800

    add cgi program

---
+++ ss/cgi-bin/post_auth.cpp
@@ -0,0 +1,26 @@
+#include "wrap.h"
+#include "parse.h"
+
+int main() {
+    char *buf,*p;
+    int length=0;
+    char content[MAXLINE],data[MAXLINE];
+
+
+    if ((buf = getenv("CONTENT-LENGTH")) != NULL)
+    {
+    	length=atol(buf);
+    }
+
+    p=fgets(data,length+1,stdin);
+    if(p==NULL)
+    	sprintf(content, "Something is wrong\r\n");
+    else
+	sprintf(content, "Info:%s\r\n",data);
+
+    printf("Content-length: %d\r\n", strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+    exit(0);
+}
